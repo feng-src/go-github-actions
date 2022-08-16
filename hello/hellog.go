b7b52335aa7efec9dde9d85e479cafbc109665b2
feng-src <1195494025@qq.com>
2022-08-16 03:13:18 +0000

    test

---
+++ hello/hellog.go
@@ -0,0 +1,9 @@
+package hello
+
+// Greet... Greet GitHub Actions
+
+func Greet() string {
+   cda
+    return "Hello GitHub Actions"
+
+}
---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+
+    "fmt"
+
+    "github.com/feng-src/go-github-actions/hello"
+
+)
+
+func main() {
+
+    fmt.Println(hello.Greet())
+
+}
